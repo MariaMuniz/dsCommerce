b46374892e71073c7e832a40b844c0fd509b7b07
MariaMuniz <cidamuniz2011@yahoo.com.br>
2023-08-09 17:26:34 -0300

    Bean Validation

--- src/main/java/com/projeto/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/projeto/dscommerce/dto/ProductDTO.java
@@ -4,12 +4,25 @@ package com.projeto.dscommerce.dto;
 
 import com.projeto.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 
 	   private Long id;
+	   @Size(min= 3, max =80, message= "Nome deve ter entre 3 e 80 caracteres" )
+	   @NotNull
+	   @NotBlank(message= "Campo requerido")
 	    private String name;
+	   
+	   @Size(min= 10, message= "Descrição deve ter no mínimo 10 caracteres" )
+	   @NotBlank(message= "Campo requerido")
 		private String description;
+	   @Positive(message= "preço deve ser positivo")
 		private Double price;
+	   
 		private String imgUrl;
 		
 
